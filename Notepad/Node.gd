cd05ebf900bcad2b6bdda9b7aaeec2f71803b606
Danny Byus <dakunesu@gmail.com>
2022-05-12 13:38:09 -0700

    Add files via upload
    Fixed file saving, added new and open dialogues.

--- Notepad/Node.gd
+++ Notepad/Node.gd
@@ -1,21 +1,59 @@
 extends Node
 
+var current_file := ''
+
+const menu_items = ['New', 'Open']
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var save_game = File.new()
-	if not save_game.file_exists('user://textdata'):
-		return
+	for m in menu_items:
+		$MenuButton.get_popup().add_item(m)
 
-	save_game.open('user://textdata', File.READ)
+	$MenuButton.get_popup().connect("id_pressed", self, "_on_item_pressed")
 
-	$Notepad.text = save_game.get_line()
+func _on_item_pressed(id):
+	var item = menu_items[id]
 
-	save_game.close()
+	if item == 'New':
+		$SaveDialog.current_dir = 'user://'
+		$SaveDialog.popup()
+	elif item == 'Open':
+		$OpenDialog.current_dir = 'user://'
+		$OpenDialog.popup()
 
 func _on_Notepad_text_changed():
-	var save_game = File.new()
-	save_game.open('user://textdata', File.WRITE)
+	if current_file == '':
+		return
 
-	save_game.store_line($Notepad.text)
+	if $Notepad.text.strip_edges() == '':
+		var dir = Directory.new()
+		dir.remove(current_file)
+		return
+
+	var save_game := File.new()
+	save_game.open(current_file.strip_edges(), File.WRITE)
+
+	save_game.store_string($Notepad.text)
 
 	save_game.close()
+
+func _on_FileDialog_file_selected(path):
+	current_file = path
+
+	var save_game = File.new()
+	if not save_game.file_exists(path):
+		return
+
+	save_game.open(path, File.READ)
+
+	$Notepad.text = ''
+
+	while not save_game.eof_reached():
+		$Notepad.text += save_game.get_line() + '\n'
+
+	save_game.close()
+
+
+func _on_SaveDialog_file_selected(path):
+	current_file = path
+	$Notepad.text = ''
